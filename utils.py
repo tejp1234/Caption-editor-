ff717fbcc3119bbbd2358e7f5019937d22f16cc9
tejp1234 <always.growwithbro@gmail.com>
2025-07-24 19:33:09 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
+# utils.py
 from pyrogram.types import Message
 
 async def edit_or_reply(message: Message, text: str):
-    try:
-        await message.edit_text(text)
-    except:
-        await message.reply(text)
+    if message.from_user:
+        if message.reply_to_message:
+            await message.reply(text)
+        else:
+            await message.reply(text)
